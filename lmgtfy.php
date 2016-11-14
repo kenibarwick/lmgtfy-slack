5736aadf34a7333be8a0f307448a9fb613d8b212
Keni <kenibarwick@gmail.com>
2016-11-14 15:36:10 +0000

    Update lmgtfy.php

--- lmgtfy.php
+++ lmgtfy.php
@@ -1,6 +1,6 @@
 <?php
 
-$user_agent = "lmgtfy/1.0 (https://github.com/kenibarwick/lmgtfy; kenibarwick@gmail.com)";
+$user_agent = "lmgtfy/1.0 (https://github.com/kenibarwick/lmgtfy-slack; kenibarwick@gmail.com)";
 $command = $_POST['command'];
 $search = $_POST['text'];
 $token = $_POST['token'];
@@ -10,33 +10,38 @@ if($token != 'dMhNdMFR1atYtBLjIZ8iP3ty'){
     die($msg);
     echo $msg;
 }
-$url_to_check = "http://lmgtfy.com/?q=".$search;
 
-// As we are only needing to create the URL for now... 
-// But later to shorten the url you can use - https://developers.google.com/url-shortener/v1/getting_started
-// curl https://www.googleapis.com/urlshortener/v1/url \
-//  -H 'Content-Type: application/json' \
-//  -d '{"longUrl": "http://www.google.com/"}'
+$longUrl = "http://lmgtfy.com/?q=".$search;
+$postData = array('longUrl' => $longUrl);
+$jsonData = json_encode($postData);
 
-// $ch = curl_init($url_to_check);
-// curl_setopt ($ch, CURLOPT_HTTPHEADER, Array("Content-Type: application/json")); 
-// curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// $ch_response = curl_exec($ch);
-// curl_close($ch);
-// $response_array = json_decode($ch_response, TRUE);
-// $response_array['id']
-// $response_array['status_code']
+$curlObj = curl_init();
+
+curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAqk5yBchMhkzTX8Q8_6Ryi-C8FmqwDAhU');
+curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curlObj, CURLOPT_HEADER, 0);
+curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+curl_setopt($curlObj, CURLOPT_POST, 1);
+curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+
+$response = curl_exec($curlObj);
+
+// Change the response json string to object
+$json = json_decode($response);
+
+curl_close($curlObj);
+
 
 if($ch_response === FALSE){
 
-  # isitup.org could not be reached 
+  # lmgtfy.org could not be reached 
   $reply = "The world is dying as Google could not be reached.";
 
 } else {
 
 		// $reply = "Have a look here for your answer ".$response_array["$url_to_check"];
-		$reply = "Have a look here for your answer: *<".$url_to_check.">.*";
+		$reply = "You'll find your answer here: *<".$json->id.">.*";
 		}
 
 echo $reply;
